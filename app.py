b50766239e8597e9d1ef03d0b6842818da65ff48
tuyanshuai <tuyanshuai@gmail.com>
2019-11-05 20:32:01 -0700

    slide

--- app.py
+++ app.py
@@ -1,5 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify, request
 app = Flask(__name__, static_url_path="", static_folder="static")
 @app.route('/')
 def index():    
     return render_template('index.html')
+
+@app.route('/next', methods=['POST'])
+def next():
+    history = request.args.get('history', '')
+
+    # Recommand according the history
+    videostr = {'url': history[-1]+'1.mp4'}
+    return jsonify(videostr)
+
+
+
+if __name__ =='main':
+    app.run(debug=True)
